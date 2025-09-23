5c85941dec4cd5003ea716e2d43b682820774dda
mail2mathewpeter <123640890+mail2mathewpeter@users.noreply.github.com>
2025-09-23 21:21:00 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from app import app
 if __name__ == "__main__":
     # Use Render's PORT env variable (fallback to 5000 for local)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="127.0.0.1", port=port)
