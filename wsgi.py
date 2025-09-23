1da078cb640842f04a43c7095f36315b0a277bb3
mail2mathewpeter <123640890+mail2mathewpeter@users.noreply.github.com>
2025-09-23 20:40:33 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,4 @@
-from app import app as application
+from app import app
 
-# For local debug via: `python server/wsgi.py`
 if __name__ == "__main__":
-  application.run(host="0.0.0.0", port=5000)
-
+    app.run(host="0.0.0.0", port=5000)
